4181614ecaad6486d46fe78e9c16a2ebaba934f0
harnoordhindsa-git <harnoor.dhindsa@outlook.com>
2025-04-14 08:11:38 -0600

    Update client.py

--- examples/bank_account_fraud_example/client.py
+++ examples/bank_account_fraud_example/client.py
@@ -1,5 +1,6 @@
 import argparse
 from pathlib import Path
+import os
 
 import flwr as fl
 import torch
@@ -12,40 +13,58 @@ from torch.utils.data import DataLoader
 from examples.models.mlp_classifier import MLPNet
 from fl4health.clients.basic_client import BasicClient
 from fl4health.utils.config import narrow_dict_type
-from fl4health.utils.load_data import load_data
+from fl4health.utils.load_data import DataPrep
 from fl4health.utils.metrics import Accuracy, BalancedAccuracy, F1
-
 from fl4health.utils.class_weights import compute_class_counts
 
 class FraudClient(BasicClient):
+    def __init__(self, data_file_path: str, scaler_path: str, metrics: list, device: torch.device):
+        super().__init__(data_path=data_file_path, metrics=metrics, device=device)
+        self.data_file_path = data_file_path
+        self.scaler_path = scaler_path
+        self.device = device
+        self.metrics = metrics
+        
+        self.data_loader = DataPrep(data_file_path=Path(self.data_file_path), scaler_path=Path(self.scaler_path))
+        self.input_dim = self.data_loader.get_input_dim()
+
+        self.model = self.get_model({})
+        self.initialized = True
+        print(self.input_dim)     
+
     def get_data_loaders(self, config: Config) -> tuple[DataLoader, DataLoader]:
         batch_size = narrow_dict_type(config, "batch_size", int)
-        train_loader, val_loader, _ = load_data(self.data_path, batch_size)
+        self.train_loader = self.data_loader.train_loader(batch_size)
+        train_loader, val_loader, _, _, _ = self.data_loader.load_baf_data(batch_size=batch_size)
         return train_loader, val_loader
 
     def get_test_data_loader(self, config: Config) -> DataLoader | None:
-        return None
+        batch_size = narrow_dict_type(config, "batch_size", int)
+        _, _, test_loader, _, _ = self.data_loader.load_baf_data(batch_size=batch_size)
+        return test_loader
     
     def get_criterion(self, config: Config) -> _Loss:
-        counts = compute_class_counts(self.data_path)
-        pos_weight = torch.tensor([counts[0] / counts[1]])  # more fraud penalty
+        counts = compute_class_counts(self.data_file_path)
+        pos_weight = torch.tensor([counts[0] / counts[1]]) 
         return torch.nn.BCEWithLogitsLoss(pos_weight=pos_weight.to(self.device))
 
     def get_optimizer(self, config: Config) -> Optimizer:
         return torch.optim.Adam(self.model.parameters(), lr=0.001)
 
     def get_model(self, config: Config) -> nn.Module:
-        input_dim = 68  # Adjust based on actual processed feature size
-        return MLPNet(input_dim = input_dim).to(self.device)
+        return MLPNet(input_dim = self.input_dim).to(self.device)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="FL Client Main")
-    parser.add_argument("--dataset_path", action="store", type=str, help="Path to the local dataset")
     args = parser.parse_args()
-
+    
+    data_path = os.environ.get("data_file_path")
+    scaler_path = os.environ.get("scaler_save_path")
+    print(f"Using data path: {data_path}")
+    print(f"Using scaler path: {scaler_path}")
+    
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    data_path = Path(args.dataset_path)
-    print('\n', data_path)
-    client = FraudClient(data_path, [BalancedAccuracy("balanced_accuracy"), F1("f1_score")], device)
+    
+    client = FraudClient(data_file_path = data_path, scaler_path = scaler_path, metrics = [BalancedAccuracy("balanced_accuracy"), F1("f1_score")], device = device)
     fl.client.start_client(server_address="0.0.0.0:8080", client=client.to_client())
     client.shutdown()
