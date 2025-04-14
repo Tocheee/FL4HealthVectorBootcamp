6dcf6c5160bf432f7dcb11de0f175cd38acf1a6a
Tochi_Okeke <32921464+Tocheee@users.noreply.github.com>
2025-04-14 13:55:40 -0600

    Update mlp_classifier.py
    Update mlp_classifier.py to include the new model BinaryModel

--- examples/models/mlp_classifier.py
+++ examples/models/mlp_classifier.py
@@ -1,7 +1,8 @@
 import torch
+import torch.nn as nn
 
 
-class MLP(torch.nn.Module):
+class MLP(nn.Module):
     def __init__(self, input_dim: int, output_dim: int) -> None:
         super().__init__()
         self.linear = torch.nn.Linear(input_dim, output_dim)
@@ -9,3 +10,26 @@ class MLP(torch.nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         outputs = torch.sigmoid(self.linear(x))
         return outputs
+
+
+
+class BinaryModel(nn.Module):
+    def __init__(self, input_dim: int):
+        super(BinaryModel, self).__init__()
+        self.fc1 = nn.Linear(input_dim, 256)
+        self.bn1 = nn.BatchNorm1d(256)
+        self.fc2 = nn.Linear(256, 128)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.fc3 = nn.Linear(128, 64)
+        self.bn3 = nn.BatchNorm1d(64)
+        self.out = nn.Linear(64, 1)
+
+        self.act = nn.GELU()  # Better for deeper networks
+        self.dropout = nn.Dropout(0.3)
+
+    def forward(self, x):
+        x = self.dropout(self.act(self.bn1(self.fc1(x))))
+        x = self.dropout(self.act(self.bn2(self.fc2(x))))
+        x = self.dropout(self.act(self.bn3(self.fc3(x))))
+        x = self.out(x).reshape(-1)
+        return x
