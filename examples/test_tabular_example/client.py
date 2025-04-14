4a971e1c994df31d9adc44cf2759e44716fd6e0d
Tochi_Okeke <32921464+Tocheee@users.noreply.github.com>
2025-04-14 13:59:36 -0600

    test_tabular_example with BAF base dataset

---
+++ examples/test_tabular_example/client.py
@@ -0,0 +1,51 @@
+import argparse
+from pathlib import Path
+
+import flwr as fl
+import torch
+import torch.nn as nn
+from flwr.common.typing import Config
+from torch.nn.modules.loss import _Loss
+from torch.optim import Optimizer
+from torch.utils.data import DataLoader
+
+from examples.models.mlp_classifier import BinaryModel
+from fl4health.clients.basic_client import BasicClient
+from fl4health.utils.config import narrow_dict_type
+from fl4health.utils.load_baf_data import load_baf_data, load_baf_test_data
+from fl4health.utils.metrics import Accuracy
+
+
+class TabularClient(BasicClient):
+
+        def get_data_loaders(self, config: Config) -> tuple[DataLoader, DataLoader]:
+            batch_size = narrow_dict_type(config, "batch_size", int)
+            train_loader, val_loader, _ = load_baf_data(self.data_path, batch_size)
+            return train_loader, val_loader
+
+        def get_test_data_loader(self, config: Config) -> DataLoader | None:
+            batch_size = narrow_dict_type(config, "batch_size", int)
+            test_loader, _ = load_baf_test_data(self.data_path, batch_size)
+            return test_loader
+
+        def get_criterion(self, config: Config) -> _Loss:
+            return torch.nn.BCEWithLogitsLoss()
+
+        def get_optimizer(self, config: Config) -> Optimizer:
+            return torch.optim.SGD(self.model.parameters(), lr=0.01)
+
+        def get_model(self, config: Config) -> nn.Module:
+            return BinaryModel(28).to(self.device)
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="FL Client Main")
+    parser.add_argument("--dataset_path", action="store", type=str, help="Path to the local dataset")
+    args = parser.parse_args()
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    data_path = Path(args.dataset_path)
+    client = TabularClient(data_path, [Accuracy("accuracy")], device)
+    fl.client.start_client(server_address="0.0.0.0:8080", client=client.to_client())
+    client.shutdown()
+
